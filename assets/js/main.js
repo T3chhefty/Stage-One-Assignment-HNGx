d9bfa4f89a8399847d896cf49380192227a2963f
T3chhefty <shadrachndu85@gmail.com>
2023-09-08 07:08:27 +0100

    my first commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+
+
+
+//Date and Time functions
+const time = document.querySelector("#time")
+const date = document.querySelector("#date")
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+
+setInterval(myTimer, 1000)
+
+function myTimer() {
+    const currentTime = new Date();
+    let day = weekday[currentTime.getDay()];
+    time.innerHTML = currentTime.toLocaleTimeString();
+    date.innerHTML = day // + " " +  currentTime.toLocaleDateString();
+ }
